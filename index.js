64a911f41367bd770b711d0e0c90df8f6eebf925
Nguyễn Duy Hà <ha8893536@gmail.com>
2026-05-26 04:21:46 +0700

    Add index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Route mặc định
+// Phục vụ các file tĩnh (html, css, js, hình ảnh, âm thanh...) từ thư mục hiện hành
+app.use(express.static(__dirname));
+
+// Route mặc định phục vụ file index.html
 app.get('/', (req, res) => {
-    res.send('🎉 Happy Birthday App đang chạy trên Render!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Lắng nghe cổng
